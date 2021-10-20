6720837cc900cf21339f837b832f999b99c16245
Felipe Cruz Castrillon <fcfelipecruz05@gmail.com>
2021-10-19 21:30:48 -0500

    Actualizar Actividad_7

---
+++ Todos los cortes/Corte_2/Actividad_7/Ejercicio_3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        unsafe static int * devolverApuntador(Queue listaEnteros,int * direccion)
+        {
+            
+            int * puntero;
+            foreach (int num in listaEnteros)
+            {
+                if (num==*direccion)
+                {
+                    puntero=&num;
+                    return puntero;
+                }
+            }
+            return null;
+            
+        }
+
+        static void Main(string[] args)
+        {
+            bool salir=false;
+            Queue listaEnteros=new Queue();
+
+            while (salir==false)
+            {
+                Console.WriteLine("Digite un numero entero, Para dejar de llenar la lista ingrese el numero 0 ");
+                int num=int.Parse(Console.ReadLine());
+                if(num==0)
+                {
+                    salir=true;
+                }
+                else
+                {
+                    listaEnteros.Enqueue(num);
+                }
+                
+            }
+            unsafe
+            {
+                int primerNumero = (int) listaEnteros.Peek();
+                int * puntero=devolverApuntador(listaEnteros,&primerNumero);
+
+                Console.Write(*puntero);
+                Console.Write(" Es el primer elemento de la cola");
+                
+            }
+            
+
+        }
+    }
+}
